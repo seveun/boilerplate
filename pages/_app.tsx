d0867704d5b3f184d1f2ab54140c8d6a4e3b67fa
matehis langlais <contact@matehis.com>
2024-05-17 18:56:30 +0200

    wip

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,19 @@
 import { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
 import { UserProvider } from "../context/UserContext";
 import { appWithTranslation } from "next-i18next";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Hydrate } from "react-query/hydration";
 import { useState } from "react";
-import "../styles/index.scss";
+import "@/styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
-        <ChakraProvider>
-          <UserProvider>
-            <Component {...pageProps} />
-          </UserProvider>
-        </ChakraProvider>
+        <UserProvider>
+          <Component {...pageProps} />
+        </UserProvider>
       </Hydrate>
     </QueryClientProvider>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,47 +1,56 @@
-import { Box, Button, Flex, Heading, Text } from "@chakra-ui/react";
 import { useUser } from "@/context/UserContext";
 import { useAuth } from "@/hooks/useAuth";
+import { Button } from "@/components/ui/button"; // Assure-toi que l'importation correspond à l'endroit où le composant Button est installé
+import clsx from "clsx";
+import { twMerge } from "tailwind-merge";
+import { FcGoogle } from "react-icons/fc";
 
 const IndexPage = () => {
   const { user } = useUser();
   const { handleGoogleLogin, handleLogout } = useAuth();
+
   return (
-    <Box
-      h="100vh"
-      w="100%"
-      display="flex"
-      flexDirection="column"
-      alignItems="center"
-      justifyContent="center"
-      bg="gray.800"
-      color="gray.100"
-    >
-      <Heading as="h1" size="2xl" mb={4} letterSpacing="wider">
+    <div className="h-screen w-full flex flex-col items-center justify-center bg-gray-800 text-gray-100">
+      <h1 className="text-4xl font-bold mb-4 tracking-wider">
         Boilerplate Deployed
-      </Heading>
-      <Text fontSize="xl" mb={8}>
-        Express + Sequelize + Next.js + Chakra UI + Firebase.
-      </Text>
-      <Box pb="20px">
-        {user ? (
-          <Flex align="center" direction="column">
-            <Box fontWeight="bold" color="green">
-              Connected with {user.email}
-            </Box>
-            <Button mt="1vw" onClick={handleLogout}>
-              Se deconnecter
-            </Button>
-          </Flex>
-        ) : (
-          <Flex align="center" direction="column">
-            <Button onClick={handleGoogleLogin}>Connect with Google</Button>
-            <Box fontWeight="bold" color="red" mt="1vw">
-              Disconnected
-            </Box>
-          </Flex>
+      </h1>
+      <p
+        className={twMerge(
+          clsx(
+            "text-xl mb-8 px-10 text-center",
+            user ? "text-green-500" : "text-red-500",
+          ),
         )}
-      </Box>
-    </Box>
+      >
+        Fastify + Serverless + Sequelize + Next.js + Shadcn + Firebase.
+      </p>
+      <div className="pb-5">
+        {user ? (
+          <div className="flex flex-col items-center">
+            <div className={"font-bold"}>Connected with {user.email}</div>
+            <Button
+              className="mt-4"
+              variant="destructive"
+              onClick={handleLogout}
+            >
+              Se déconnecter
+            </Button>
+          </div>
+        ) : (
+          <div className="flex flex-col items-center">
+            <Button
+              className={twMerge(
+                "flex items-center bg-white text-gray-800 font-semibold py-2 px-4 border border-gray-300 rounded shadow-sm hover:bg-gray-100",
+              )}
+              onClick={handleGoogleLogin}
+            >
+              <FcGoogle className="mr-2 scale-125" />
+              Connect with Google
+            </Button>
+          </div>
+        )}
+      </div>
+    </div>
   );
 };
 
